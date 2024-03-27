37628177e7967311d65a8c6fbae6dbdd807db54d
deblandero <olivier@deblandero.be>
2024-03-27 20:44:39 +0100

    Add fix style index + new page RPD and the first class

---
+++ assets/rpg/classes/profile.ts
@@ -0,0 +1,4 @@
+class Profil {
+    name: string;
+}
+
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,15 +9,16 @@
       <SvgComputerMobile class="md:hidden w-64" />
     </div>
     <!-- <div class="col-span-4 md:col-span-1 flex items-center flex-col pt-24 px-4 sm:pr-16 md:pr-32"> -->
-    <div class="absolute w-1/2 right-24 text-right">
+    <div class="absolute w-1/2 pr-32 right-24 flex-col items-end text-end">
       <p class="w-full text-3xl">DEBLANDER OLIVIER</p>
       <p class="w-full text-xl font-extrabold">webdeveloper.</p>
-      <div class="w-full flex items-start">
+      <div class="w-full flex justify-end">
         <button @click="mailToOD()">
           <NuxtImg src="/images/post-office.png" width="25px"/>
         </button>
+        <NuxtLink to="rpg" class="pl-4">RPG</NuxtLink>
       </div>
-      <p class="w-1/2 text-xl pt-4">Explorateur du code et créateur digital, je façonne des mondes en ligne à travers la magie du développement web.</p>
+      <p class="w-ful text-xl pt-4 text-end pl-32">Explorateur du code et créateur digital, je façonne des mondes en ligne à travers la magie du développement web.</p>
     </div>
   </div>
   <CopyrightText/>
